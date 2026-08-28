d4df8557731d318ac1978dc887decccd47266fd9
codeWithReya <shreyasharma91004@gmail.com>
2026-08-28 12:04:49 +0530

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,10 +4,14 @@ import mysql.connector
 
 def get_database():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="student_registration"
+        host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
+        port=4000,
+        user="5WtwPzksksDPRZX.root",
+        password="N5sodO8ZaMzPg4yJ",
+        database="student_registration",
+        ssl_ca="ca cert.pem",
+        ssl_verify_cert=True,
+        ssl_verify_identity=True
     )
 
 # PAGE CONFIG
